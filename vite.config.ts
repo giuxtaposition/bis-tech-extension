a97b0f1546e48dbe0079ee10fd793fbe1a7081a6
giuxtaposition <yg97.cs@gmail.com>
2023-12-22 17:46:16 +0100

    ci: run tests

--- vite.config.ts
+++ vite.config.ts
@@ -30,6 +30,7 @@ export default defineConfig(({ mode }) => {
       globals: true,
       environment: "jsdom",
       setupFiles: "./src/tests/setup.ts",
+      include: ["./src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     },
   };
 });
